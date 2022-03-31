47f17857ed95d0cb573be0b31dc8ad496dd579b8
KELMAN NICOLAS <47297850@ort>
2022-03-31 08:52:16 -0300

    Exercise 2 and 3

---
+++ Assets/Script/Ejercicio03.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio03 : MonoBehaviour
+{
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Buen dia " + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
