b44a67cf7c62a06004e291c4fba67ccec8b16664
KELMAN NICOLAS <47297850@ort>
2022-04-05 10:28:06 -0300

    Realizado Ej05

--- Assets/Script/Ejercicio04.cs
+++ Assets/Script/Ejercicio04.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Ejercicio04 : MonoBehaviour
 {
+    public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + (num1 + num2));
     }
 
     // Update is called once per frame
--- Assets/Script/Ejercicio05.cs
+++ Assets/Script/Ejercicio05.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Ejercicio05 : MonoBehaviour
 {
+    public int num1;
+    public int num2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(num1 + " / " + num2 + " = " + num1 / num2);
     }
 
     // Update is called once per frame
